e1d7ec7e221056ed8e7456ed67a202851b5b5f25
32494 <amine.nemmaoui@student.vinci.be>
2021-11-28 23:33:27 +0100

    avancement

--- src/ApplicationCentrale.java
+++ src/ApplicationCentrale.java
@@ -1,8 +1,16 @@
 import java.sql.*;
 
 public class ApplicationCentrale {
+    private static PreparedStatement ajouterUneUe;
+    private static PreparedStatement ajouterPrerequis;
+    private static PreparedStatement ajouterEtudiant;
+    private static PreparedStatement ajouterUeValide;
+    private static PreparedStatement visualiserEtudiantBloc;
+    private static PreparedStatement visualiserToutEtudiants;
+    private static PreparedStatement visualiserEtudiantsPaeNonValide;
+    private static PreparedStatement visualiserUeBloc;
 
-    public static void test() {
+    public ApplicationCentrale() {
 
         try {
             Class.forName("org.postgresql.Driver");
@@ -21,21 +29,129 @@ public class ApplicationCentrale {
         }
 
         try {
-            PreparedStatement ajouterUneUe = conn.prepareStatement("SELECT * FROM projet.ajouterUnUe(?, ?, ?, ?);");
-            PreparedStatement ajouterPrerequis = conn.prepareStatement("SELECT * FROM projet.ajouterPrerequis(?, ?);");
-            PreparedStatement ajouterEtudiant = conn.prepareStatement("SELECT * FROM projet.ajouterEtudiant(?, ?, ?, ?);");
-            PreparedStatement ajouterUeValide = conn.prepareStatement("SELECT * FROM projet.ajouterUeValide(?, ?);");
-            PreparedStatement visualiserEtudiantBloc = conn.prepareStatement("SELECT e.nom, e.prenom, e.credits_pae FROM projet.etudiants e WHERE e.bloc = ? ORDER BY e.credits_pae;");
-            PreparedStatement visualiserToutEtudiants = conn.prepareStatement("SELECT e.nom, e.prenom, e.bloc, e.credits_pae FROM projet.etudiants e ORDER BY e.credits_pae;");
-            PreparedStatement visualiserEtudiantsPaeNonValide = conn.prepareStatement("SELECT e.nom, e.prenom, e.credits_valides FROM projet.etudiants e WHERE e.pae_valide = 0;");
-            PreparedStatement visualiserUeBloc = conn.prepareStatement("SELECT u.code_ue, u.titre, u.nombre_inscrits FROM projet.ues u WHERE u.bloc = ? ORDER BY u.nombre_inscrits;");
+            ajouterUneUe = conn.prepareStatement("SELECT * FROM projet.ajouterUneUe(?, ?, ?, ?);");
+            ajouterPrerequis = conn.prepareStatement("SELECT * FROM projet.ajouterPrerequis(?, ?);");
+            ajouterEtudiant = conn.prepareStatement("SELECT * FROM projet.ajouterEtudiant(?, ?, ?, ?);");
+            ajouterUeValide = conn.prepareStatement("SELECT * FROM projet.ajouterUeValide(?, ?);");
+            visualiserEtudiantBloc = conn.prepareStatement("SELECT e.nom, e.prenom, e.credits_pae FROM projet.etudiants e WHERE e.bloc = ? ORDER BY e.credits_pae;");
+            visualiserToutEtudiants = conn.prepareStatement("SELECT e.nom, e.prenom, e.bloc, e.credits_pae FROM projet.etudiants e ORDER BY e.credits_pae;");
+            visualiserEtudiantsPaeNonValide = conn.prepareStatement("SELECT e.nom, e.prenom, e.credits_valides FROM projet.etudiants e WHERE e.pae_valide = 0;");
+            visualiserUeBloc = conn.prepareStatement("SELECT u.code_ue, u.titre, u.nombre_inscrits FROM projet.ues u WHERE u.bloc = ? ORDER BY u.nombre_inscrits;");
         } catch (SQLException e) {
             System.out.println(e.getMessage().split("\n")[0]);
             System.exit(1);
         }
     }
 
+    public void ajouterUneUe(String code, String nom, int bloc, int nbrCred) {
+        try {
+            ajouterUneUe.setString(1, code);
+            ajouterUneUe.setString(2,nom);
+            ajouterUneUe.setInt(3,bloc);
+            ajouterUneUe.setInt(4,nbrCred);
+            ajouterUneUe.executeQuery();
+            System.out.println("L'UE a été ajouté avec succès");
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("L'UE n'a pas été ajouté");
+        }
+    }
 
+    public void ajouterPrerequis(String code1, String code2) {
+        try {
+            ajouterPrerequis.setString(1,code1);
+            ajouterPrerequis.setString(2,code2);
+            ajouterPrerequis.executeQuery();
+            System.out.println("Le prérequis a bien été ajouté à l'UE");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Le prérequis n'a pas été ajouté à l'UE");
+        }
+    }
+
+    public void ajouterEtudiant(String nom, String prenom, String email, String mdp) {
+        try {
+            ajouterEtudiant.setString(1,nom);
+            ajouterEtudiant.setString(2,prenom);
+            ajouterEtudiant.setString(3,email);
+            ajouterEtudiant.setString(4,mdp);
+            ajouterEtudiant.executeQuery();
+            System.out.println("L'étudiant a bien été ajouté");
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("L'étudiant n'a pas été ajouté");
+        }
+    }
+
+    public void ajouterUeValide(String email, String codeUe) {
+        try {
+            ajouterUeValide.setString(1,email);
+            ajouterUeValide.setString(2,codeUe);
+            ajouterUeValide.executeQuery();
+            System.out.println("L'étudiant a bien validé cette UE");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Verfier si vous avez correctement écrit l'email ou le code de l'UE");
+        }
+    }
+
+    public void visualiserEtudiantBloc(int bloc) {
+        try {
+            visualiserEtudiantBloc.setInt(1,bloc);
+            try (ResultSet rs = visualiserEtudiantBloc.executeQuery()) {
+                while(rs.next()) {
+                    System.out.println("Nom de l'étudiant : " + rs.getString(1) + "\nPrénom de l'étudiant : " + rs.getString(2)
+                    + "\nNombre de crédit PAE : " + rs.getInt(3));
+                    System.out.println();
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void visualiserToutEtudiants() {
+        try {
+            try(ResultSet rs = visualiserToutEtudiants.executeQuery()) {
+                while(rs.next()) {
+                    System.out.println("Nom de l'étudiant : " + rs.getString(1) + "\nPrénom de l'étudiant : " + rs.getString(2)
+                    + "\nBloc : " + rs.getInt(3) + "\nNombre de crédit PAE : " + rs.getInt(4));
+                    System.out.println();
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void visualiserEtudiantsPaeNonValide() {
+        try {
+            try(ResultSet rs = visualiserEtudiantsPaeNonValide.executeQuery()) {
+                while(rs.next()) {
+                    System.out.println("Nom de l'étudiant : " + rs.getString(1) + "\nPrénom de l'étudiant : " + rs.getString(2)
+                            + "\nNombre de crédit validés : " + rs.getInt(3));
+                    System.out.println();
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void visualiserUeBloc(int bloc) {
+        try {
+            visualiserUeBloc.setInt(1,bloc);
+            try (ResultSet rs = visualiserUeBloc.executeQuery()) {
+                while(rs.next()) {
+                    System.out.println("Code de l'UE : " + rs.getString(1) + "\nNom de l'UE : " + rs.getString(2)
+                    + "\nNombre d'inscrits : " + rs.getInt(3));
+                    System.out.println();
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
 
 
--- src/TestAppCentrale.java
+++ src/TestAppCentrale.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class TestAppCentrale {
     private final static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) {
-        ApplicationCentrale app = new ApplicationCentrale();
-        app.test();
+    private final static ApplicationCentrale app = new ApplicationCentrale();
 
-        System.out.println("Bienvenue sur l'application étudiants");
+    public static void main(String[] args) {
+        System.out.println("Bienvenue sur l'application centrale");
         System.out.println("Quel action voulez vous effectuer ?");
         int choix = 0;
         do {
@@ -19,31 +18,102 @@ public class TestAppCentrale {
             System.out.println("6 -> visualiser tout les étudiants");
             System.out.println("7 -> visualiser tout les étudiants qui n'ont pas validé leur PAE");
             System.out.println("8 -> visualiser toutes les UE d'un bloc particulier");
-
             System.out.println();
             System.out.print("Votre choix : ");
             choix = scanner.nextInt();
             scanner.nextLine();
             switch (choix) {
                 case 1:
+                    ajouterUe();
                     break;
                 case 2:
+                    ajouterPrerequis();
                     break;
                 case 3:
+                    ajouterEtudiant();
                     break;
                 case 4:
+                    ajouterUeValide();
                     break;
                 case 5:
+                    visualiserEtudiantBloc();
                     break;
                 case 6:
+                    visualiserToutEtudiants();
                     break;
                 case 7:
+                    visualiserEtudiantsPaeNonValide();
                     break;
                 case 8:
+                    visualiserUeBloc();
                     break;
             }
 
         } while (choix >= 1 && choix <= 8);
 
+    }
+
+    private static void ajouterUe(){
+        System.out.print("Veuillez entrer le code de l'UE : ");
+        String code = scanner.nextLine();
+        System.out.print("Veuillez entrer le nom de cette UE : ");
+        String nom = scanner.nextLine();
+        System.out.print("Veuillez entrer le bloc de cette UE : ");
+        int bloc = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Veuillez entrer le nombre de crédits de cette UE : ");
+        int nbrCred = scanner.nextInt();
+        scanner.nextLine();
+        app.ajouterUneUe(code,nom,bloc,nbrCred);
+    }
+
+    private static void ajouterPrerequis(){
+        System.out.print("Veuillez entrer le code de l'UE : ");
+        String code1 = scanner.nextLine();
+        System.out.print("Veuillez entrer le code de son prérequis : ");
+        String code2 = scanner.nextLine();
+        app.ajouterPrerequis(code1,code2);
+    }
+
+    private static void ajouterEtudiant(){
+        System.out.print("Veuillez entrer le nom de l'étudiant : ");
+        String nom = scanner.nextLine();
+        System.out.print("Veuillez entrer le prénom de l'étudiant : ");
+        String prenom = scanner.nextLine();
+        System.out.print("Veuillez entrer l'email de l'étudiant : ");
+        String email = scanner.nextLine();
+        System.out.print("Veuillez entrer le mot de passe de l'étudiant : ");
+        String mdp = scanner.nextLine();
+        app.ajouterEtudiant(nom,prenom,email,mdp);
+    }
+
+    private static void ajouterUeValide(){
+        System.out.print("Veuillez entrer l'email de l'étudiant : ");
+        String email = scanner.nextLine();
+        System.out.print("Veuillez entrer le code de l'UE : ");
+        String code = scanner.nextLine();
+        app.ajouterUeValide(email,code);
+    }
+
+    private static void visualiserEtudiantBloc() {
+        System.out.print("Veuillez entrer le bloc des étudiants que vous voulez voir : ");
+        int bloc = scanner.nextInt();
+        scanner.nextLine();
+        app.visualiserEtudiantBloc(bloc);
+    }
+
+    private static void visualiserToutEtudiants() {
+        app.visualiserToutEtudiants();
+    }
+
+    private static void visualiserEtudiantsPaeNonValide() {
+        app.visualiserEtudiantsPaeNonValide();
+    }
+
+    private static void visualiserUeBloc() {
+        System.out.print("Veuillez le bloc des UEs que vous voulez voir : ");
+        int bloc = scanner.nextInt();
+        scanner.nextLine();
+        app.visualiserUeBloc(bloc);
     }
 }
