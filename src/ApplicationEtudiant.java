d6c18ce626cd3400be5ff0a5cafe65abbd890e17
32494 <amine.nemmaoui@student.vinci.be>
2021-11-29 20:49:13 +0100

    app étudiant manque le cryptage de mdp

--- src/ApplicationEtudiant.java
+++ src/ApplicationEtudiant.java
@@ -1,8 +1,17 @@
 import java.sql.*;
 
-public class ApplicationEtudiant {
 
-    public static void test() {
+public class ApplicationEtudiant {
+    private static PreparedStatement seConnecter;
+    private static PreparedStatement ajouterUneUePae;
+    private static PreparedStatement enleverUneUePae;
+    private static PreparedStatement validerPae;
+    private static PreparedStatement visualiserdiffUePae;
+    private static PreparedStatement visualiserPae;
+    private static PreparedStatement reinitialiserPae;
+
+    public ApplicationEtudiant() {
+
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
@@ -18,8 +27,107 @@ public class ApplicationEtudiant {
             System.out.println("Impossible de joindre le server !");
             System.exit(1);
         }
+
+        try {
+            seConnecter = conn.prepareStatement("SELECT * FROM projet.etudiants e WHERE e.email = ?");
+            ajouterUneUePae = conn.prepareStatement("SELECT * FROM projet.ajouterUePae(?, ?)");
+            enleverUneUePae = conn.prepareStatement("SELECT * FROM projet.enleverUePae(?, ?)");
+            validerPae = conn.prepareStatement("SELECT * FROM projet.validerPae(?)");
+            visualiserdiffUePae = conn.prepareStatement("SELECT * FROM projet.ueDispoPae u WHERE u.email = ?");
+            visualiserPae = conn.prepareStatement("SELECT * FROM projet.ueDuPae u WHERE u.email = ?");
+            reinitialiserPae = conn.prepareStatement("SELECT * FROM projet.reinitPae(?)");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
+    public boolean seConnecter(String email, String mdp) {
+        try {
+            seConnecter.setString(1,email);
+            try(ResultSet rs = seConnecter.executeQuery()) {
+                while(rs.next()) {
+                    if(rs.getString(5).equals(mdp)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        } catch (SQLException e) {
+            System.out.println("Veuillez vérifier vos données");
+            return false;
+        }
+    }
 
+    public void ajouterUneUePae(String code, String email) {
+        try {
+            ajouterUneUePae.setString(1,code);
+            ajouterUneUePae.setString(2,email);
+            ajouterUneUePae.executeQuery();
+            System.out.println("Votre UE a bien été ajouté à votre PAE");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void enleverUneUePae(String code, String email) {
+        try {
+            enleverUneUePae.setString(1,code);
+            enleverUneUePae.setString(2,email);
+            enleverUneUePae.executeQuery();
+            System.out.println("Votre UE a bien été retiré de votre PAE");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void validerPae(String email) {
+        try {
+            validerPae.setString(1,email);
+            validerPae.executeQuery();
+            System.out.println("Votre PAE a bien été validé");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void visualiserdiffUePae(String email) {
+        try {
+            visualiserdiffUePae.setString(1,email);
+            try (ResultSet rs = visualiserdiffUePae.executeQuery()) {
+                while(rs.next()) {
+                    System.out.println("Code de l'UE : " + rs.getString(2) + "\nNom de l'UE : " + rs.getString(3)
+                                       + "\nNombre de crédits : " + rs.getInt(4) + "\nBloc : " + rs.getInt(5));
+                    System.out.println();
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void visualiserPae(String email) {
+        try {
+            visualiserPae.setString(1,email);
+            try (ResultSet rs = visualiserPae.executeQuery()) {
+                while(rs.next()) {
+                    System.out.println("Code de l'UE : " + rs.getString(2) + "\nNom de l'UE : " + rs.getString(3)
+                            + "\nNombre de crédits : " + rs.getInt(4) + "\nBloc : " + rs.getInt(5));
+                    System.out.println();
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void reinitialiserPae(String email) {
+        try {
+            reinitialiserPae.setString(1,email);
+            reinitialiserPae.executeQuery();
+            System.out.println("Votre PAE a bien été réinitialiser");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
     
 }
--- src/TestAppEtudiant.java
+++ src/TestAppEtudiant.java
@@ -1,6 +1,87 @@
+import java.util.Scanner;
+
 public class TestAppEtudiant {
-    public static void main(String[] args) {
-        ApplicationEtudiant app = new ApplicationEtudiant();
-        app.test();
-    }
+        private final static Scanner scanner = new Scanner(System.in);
+        private final static ApplicationEtudiant app = new ApplicationEtudiant();
+        private static String email;
+
+        public static void main(String[] args) {
+
+            boolean estConnecte = false;
+            while(!estConnecte) {
+                System.out.print("Entrez votre email : ");
+                email = scanner.nextLine();
+                System.out.print("Entrez votre mot de passe : ");
+                String mdp = scanner.nextLine();
+                estConnecte = app.seConnecter(email,mdp);
+                System.out.println();
+            }
+
+            System.out.println("Bienvenue sur l'application étudiants");
+            System.out.println("Quel action voulez vous effectuer ?");
+            int choix = 0;
+            do {
+                System.out.println();
+                System.out.println("1 -> ajouter une UE à votre PAE");
+                System.out.println("2 -> enlever une UE à votre PAE");
+                System.out.println("3 -> valider votre PAE");
+                System.out.println("4 -> viusaliser les différents UE que vous pouvez ajouter à votre PAE");
+                System.out.println("5 -> visualiser votre PAE");
+                System.out.println("6 -> réinitialiser votre PAE");
+                System.out.println();
+                System.out.print("Votre choix : ");
+                choix = scanner.nextInt();
+                scanner.nextLine();
+                switch (choix) {
+                    case 1:
+                        ajouterUneUePae();
+                        break;
+                    case 2:
+                        enleverUneUePae();
+                        break;
+                    case 3:
+                        validerPae();
+                        break;
+                    case 4:
+                        visualiserDiffUePae();
+                        break;
+                    case 5:
+                        visualiserUePae();
+                        break;
+                    case 6:
+                        reinitialiserPae();
+                        break;
+                }
+
+            } while (choix >= 1 && choix <= 6);
+
+        }
+
+        private static void ajouterUneUePae() {
+            System.out.print("Entrez le code de l'UE que vous voulez ajouter à votre PAE : ");
+            String code = scanner.nextLine();
+            app.ajouterUneUePae(code,email);
+        }
+
+        private static void enleverUneUePae() {
+            System.out.print("Entrez le code de l'UE que vous voulez supprimer de votre PAE : ");
+            String code = scanner.nextLine();
+            app.enleverUneUePae(code,email);
+        }
+
+        private static void validerPae() {
+            app.validerPae(email);
+        }
+
+        private static void visualiserDiffUePae() {
+            app.visualiserdiffUePae(email);
+        }
+
+        private static void visualiserUePae() {
+            app.visualiserPae(email);
+        }
+
+        private static void reinitialiserPae() {
+            app.reinitialiserPae(email);
+        }
 }
